7c129851eb869b3d661eca15e2ac1437bd506893
Tanzeel Alam <tanzeelalam@gmail.com>
2024-08-15 12:17:01 +0530

    Add dog.go and go.mod

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tanzeelalam/TanzeelTM
 
 go 1.23.0
+
+require github.com/tanzeelalam/puppy v0.0.0-20240815030452-8021b6c6d09a
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/tanzeelalam/puppy"
+)
 
 func main() {
 	fmt.Println("Hello 😁")
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
 }
